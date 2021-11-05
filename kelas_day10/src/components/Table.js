aec9d818b629d42a50afad8407af3e436b938288
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-11-05 09:20:55 +0700

    convert to hooks

--- kelas_day10/src/components/Table.js
+++ kelas_day10/src/components/Table.js
@@ -1,63 +1,101 @@
-import { Component } from "react";
-class Table extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data:[{
-                id:1,
-                nama:"Adrian", 
-                umur: 22,
-                jk: "Laki-laki",
-            },
-            {   
-                id:2,
-                nama:"Caca", 
-                umur: 21,
-                jk: "Perempuan",
-            },
-        ],
-        dataForm:{
+import React, { useState } from 'react';
+function Table() {
+const [dataPenumpang,setDataPenumpang] = useState([{
+                    id:1,
+                    nama:"Adrian", 
+                    umur: 22,
+                    jk: "Laki-laki",
+                },
+                {   
+                    id:2,
+                    nama:"Caca", 
+                    umur: 21,
+                    jk: "Perempuan",
+                },
+            ],)
+
+const [penumpangBaru,setPenumpangBaru] = useState({id:0,nama:"",umur:0,jk:"",})
+
+const deleteItem = (id) => {
+    let update = dataPenumpang.filter((data) => data.id !==id);
+    setDataPenumpang(update)
+    };
+
+const changeValue = (e) => {
+        console.log(e);
+        setPenumpangBaru({...penumpangBaru, [e.target.name]:e.target.value},
+        );
+    };
+
+const handleSubmit = (e) => {
+        e.preventDefault();
+        let newData ={...penumpangBaru,id: Math.floor(Math.random()*100) +1,
+        };
+        setDataPenumpang([...dataPenumpang, newData],);
+        setPenumpangBaru({
             id:0,
-            nama: "",
+            nama:"",
             umur:0,
             jk:"",
-        },
-        }
-}      
+        },);
+    };
+
+//     constructor() {
+//         super();
+//         this.state = {
+//             data:[{
+//                 id:1,
+//                 nama:"Adrian", 
+//                 umur: 22,
+//                 jk: "Laki-laki",
+//             },
+//             {   
+//                 id:2,
+//                 nama:"Caca", 
+//                 umur: 21,
+//                 jk: "Perempuan",
+//             },
+//         ],
+//         dataForm:{
+//             id:0,
+//             nama: "",
+//             umur:0,
+//             jk:"",
+//         },
+//         }
+// }      
    
-    deleteItem = (id) => {
-        let update = this.state.data.filter((data) => data.id !==id);
-        this.setState({
-            data: update,
-        });
-    };
+//     deleteItem = (id) => {
+//         let update = this.state.data.filter((data) => data.id !==id);
+//         this.setState({
+//             data: update,
+//         });
+//     };
 
-    changeValue = (e) => {
-        console.log(e);
-        this.setState({
-            dataForm:{...this.state.dataForm, [e.target.name]:e.target.value},
-        });
-    };
+    // changeValue = (e) => {
+    //     console.log(e);
+    //     this.setState({
+    //         dataForm:{...this.state.dataForm, [e.target.name]:e.target.value},
+    //     });
+    // };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        let newData ={
-            ...this.state.dataForm,
-            id: Math.floor(Math.random()*100) +1,
-        };
-        this.setState({
-            data: [...this.state.data, newData],
-            formData:{
-                id:0,
-                nama:"",
-                umur:0,
-                jk:"",
-            },
-        });
-    };
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     let newData ={
+    //         ...this.state.dataForm,
+    //         id: Math.floor(Math.random()*100) +1,
+    //     };
+    //     this.setState({
+    //         data: [...this.state.data, newData],
+    //         formData:{
+    //             id:0,
+    //             nama:"",
+    //             umur:0,
+    //             jk:"",
+    //         },
+    //     });
+    // };
 
-
-    render(){
     return(
         <div className="container">
         <table className="table">
@@ -70,7 +108,7 @@ class Table extends Component {
             </tr>
         </thead>
         <tbody>
-        {this.state.data.map((data) =>{
+        {dataPenumpang.map((data) =>{
             return(
                 <tr key={data.id}>
                     <td>{data.id}</td>
@@ -78,34 +116,38 @@ class Table extends Component {
                     <td>{data.umur}</td>
                     <td>{data.jk}</td>
                     <td>
-                        <button className="btn btn-danger" onClick={() =>this.deleteItem(data.id)}>delete</button>
+                        <button className="btn btn-danger" 
+                        onClick={() =>deleteItem(data.id)} 
+                        >delete</button>
                     </td>
                 </tr>
             )
         })}   
         </tbody>
         </table>
-        <form onSubmit={this.handleSubmit}>
+        <form 
+        onSubmit={handleSubmit}
+        >
         <div class="mb-3">
             <label for="inputName" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="inputName" name="nama" value={this.state.dataForm.nama} onChange={(e)=> this.changeValue(e)} aria-describedby="namaHelp" />
+            <input type="text" class="form-control" id="inputName" name="nama" value={penumpangBaru.nama} onChange={(e)=> changeValue(e)} aria-describedby="namaHelp" />
             <div id="namaHelp" class="form-text">Masukan nama singkat</div>
         </div>
         <div class="mb-3">
             <label for="inputUmur" class="form-label">Umur</label>
-            <input type="age" class="form-control" id="inputUmur" name="umur" value={this.state.dataForm.umur} onChange={(e)=> this.changeValue(e)} aria-describedby="umurHelp"/>
+            <input type="age" class="form-control" id="inputUmur" name="umur" value={penumpangBaru.umur} onChange={(e)=> changeValue(e)} aria-describedby="umurHelp"/>
             <div id="umurHelp" class="form-text">Masukan umur saat ini.</div>
         </div>
         <div className="mb-3">
         <label for="inputJk" class="form-label">Jenis Kelamin</label>
             <div class="form-check">
-                <input value="Laki-Laki" class="form-check-input" type="radio" name="jk" onChange={(e)=> this.changeValue(e)} id="selectLaki"/>
+                <input value="Laki-Laki" class="form-check-input" type="radio" name="jk" onChange={(e)=> changeValue(e)} id="selectLaki"/>
                 <label class="form-check-label" for="selectLaki">
                     Laki Laki
                 </label>
             </div>
             <div class="form-check">
-            <input value="Perempuan" class="form-check-input" type="radio" name="jk" id="selectPerempuan" onChange={(e)=> this.changeValue(e)} />
+            <input value="Perempuan" class="form-check-input" type="radio" name="jk" id="selectPerempuan" onChange={(e)=> changeValue(e)} />
             <label class="form-check-label" for="selectPerempuan">
                 Perempuan
             </label>
@@ -116,6 +158,5 @@ class Table extends Component {
         </div>
     )
    }   
-}
 
 export default Table;
