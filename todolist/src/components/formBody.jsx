ae58dfffcd53fa103ec1178b992ba319f8dfc7a4
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-11-10 13:11:47 +0700

    fix bug

--- todolist/src/components/formBody.jsx
+++ todolist/src/components/formBody.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef} from 'react';
 
 function FormBody() {
+let newErrorMessage = {};
 const [data,setData] = useState(
     {
         nama:"",
@@ -9,7 +10,7 @@ const [data,setData] = useState(
         pendidikan:"",
         kelas:"",
         harapan:"",
-    })
+    });
 
 const [errorMessage,setErrorMessage] = useState({
         nama:"",
@@ -17,7 +18,7 @@ const [errorMessage,setErrorMessage] = useState({
         noHandphone:"",
         pendidikan:"",
         kelas:"",
-},)
+});
 
 const suratKesungguhan = useRef('')
 const nameRegex = /^[a-zA-Z ]*$/;
@@ -32,115 +33,122 @@ const phoneRegex = /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/;
 
     const validation = () => {
         if (data.nama === ""){
-            let nama = {...errorMessage,
+            console.log("nama kosong")
+            let nama = {...newErrorMessage,
                 nama:"Nama kosong"}
-            setErrorMessage(nama)
+            newErrorMessage = (nama)
         }else if(!nameRegex.test(data.nama)){
-            setErrorMessage({...errorMessage,
+            newErrorMessage = ({...newErrorMessage,
                 nama:"Nama tidak boleh terdapat angka"})
         }
         if (data.noHandphone ===""){
-            setErrorMessage({...errorMessage,
-                noHandphone:"Nomor kosong kosong"})
+            console.log("nomor handphone kosong")
+            newErrorMessage = ({...newErrorMessage,
+                noHandphone:"Nomor handphone kosong"})
         }else if(!phoneRegex.test(data.noHandphone)){
-            setErrorMessage({...errorMessage,
+            newErrorMessage = ({...newErrorMessage,
                 noHandphone:"Nomor telepon tidak sesuai ketentuan"})
         }
         if (data.email ===""){
-            setErrorMessage({...errorMessage,
+            console.log("email kosong")
+            newErrorMessage = ({...newErrorMessage,
                 email:"email kosong"})
         } 
         if (data.pendidikan ===""){
-            setErrorMessage({...errorMessage,
+            console.log("pendidikan kosong")
+            newErrorMessage = ({...newErrorMessage,
                 pendidikan:"pendidikan kosong"})
         }
         if (data.kelas ===""){
-            setErrorMessage({...errorMessage,
+            console.log("kelas kosong")
+            newErrorMessage = ({...newErrorMessage,
                 kelas:"kelas kosong"})
         }
-        console.log("validasi",errorMessage)
-        console.log("validasi",data)
+        console.log("validasi new message",newErrorMessage)
+        setErrorMessage(newErrorMessage);
+        console.log("validasi error message",errorMessage)
+        console.log("validasi data",data) 
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        validation()
+    const handleSubmits = (e) => {
+        validation();
         if((!(errorMessage.nama === "")) ||(!(errorMessage.email === "")) ||(!(errorMessage.noHandphone === "")) ||(!(errorMessage.pendidikan === "")) || (!(errorMessage.kelas === "")) ){
             alert("Masih ada data yang belum sesuai euy");
         }else {
             alert("Data atas nama "+ data.nama+ " berhasil diterima");
-            setData({
-                nama:"",
-                email:"",
-                noHandphone:"",
-                pendidikan:"",
-                kelas:"",
-                harapan:"",
-            },)
-            setErrorMessage({
-                nama:"",
-                email:"",
-                noHandphone:"",
-                pendidikan:"",
-                kelas:"",
-            },)
-        console.log("handle submit",errorMessage)
-        console.log("handle submit",data)
+            // setData({
+            //     nama:"",
+            //     email:"",
+            //     noHandphone:"",
+            //     pendidikan:"",
+            //     kelas:"",
+            //     harapan:"",
+            // },);
+            // setErrorMessage({
+            //     nama:"",
+            //     email:"",
+            //     noHandphone:"",
+            //     pendidikan:"",
+            //     kelas:"",
+            // },);
         }
-        
-      
+        console.log("handle submit error message",errorMessage)
+        console.log("handle submit data",data)
+        e.preventDefault();
     };
 
         return(
             <div className="p-5 border shadow" style={{'borderRadius':'30px'}}>
                 <div className="container m-1 p-2">
                     <h3>Pendaftaran Peserta Coding Bootcamp</h3>
-                    <form onSubmit={handleSubmit} className="p-2" style={{'borderRadius':'30px'}}>
-                    <div class="mb-3">
-                        <label for="InputNama" class="form-label">Nama</label>
-                        <input type="text" class="form-control" id="InputNama" placeholder="John Smith" name="nama" value={data.nama} onChange={(e)=> changeValue(e)}/>
+                    <div>{JSON.stringify(data)}</div>
+                    <div>{JSON.stringify(errorMessage)}</div>
+                    <form onSubmit={handleSubmits} className="p-2" style={{'borderRadius':'30px'}}>
+                    <div className="mb-3">
+                        <label for="InputNama" className="form-label">Nama</label>
+                        <input type="text" className="form-control" id="InputNama" placeholder="John Smith" name="nama" value={data.nama} onChange={(e)=> changeValue(e)}/>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="InputEmail" placeholder="something@mail.com" name="email" value={data.email} onChange={(e)=> changeValue(e)} />
+                        <label  className="form-label">Email</label>
+                        <input type="email" className="form-control" id="InputEmail" placeholder="something@mail.com" name="email" value={data.email} onChange={(e)=> changeValue(e)} />
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">No Handphone</label>
-                        <input type="text" class="form-control" id="InputPhone" placeholder="0812345678..." name="noHandphone" value={data.noHandphone} onChange={(e)=> changeValue(e)} />
+                        <label  className="form-label">No Handphone</label>
+                        <input type="text" className="form-control" id="InputPhone" placeholder="0812345678..." name="noHandphone" value={data.noHandphone} onChange={(e)=> changeValue(e)} />
                     </div>
                     <div class="mb-3">
-                        <label for="radio" class="form-label">Latar Belakang Pendidikan</label>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" value="IT" id="radioIt" name="pendidikan" onChange={(e)=> changeValue(e)}/>
-                            <label class="form-check-label" for="flexRadioDefault1">
+                        <label  className="form-label">Latar Belakang Pendidikan</label>
+                        <div className="form-check">
+                            <input clasName="form-check-input" type="radio" value="IT" id="radioIt" name="pendidikan" onChange={(e)=> changeValue(e)}/>
+                            <label className="form-check-label" for="flexRadioDefault1">
                                 IT
                             </label>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="pendidikan" value ="non-IT" id="radioNonIt"  onChange={(e)=> changeValue(e)}/>
-                            <label class="form-check-label" for="flexRadioDefault2">
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="pendidikan" value ="non-IT" id="radioNonIt"  onChange={(e)=> changeValue(e)}/>
+                            <label className="form-check-label" for="flexRadioDefault2">
                                 Non-IT
                             </label>
                         </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="select" class="form-label">Kelas Koding yang Dipilih</label>
-                        <select id="select" class="form-select" aria-label="Default select example" name="kelas" value={data.kelas} onChange={(e)=> changeValue(e)} >
+                    <div className="mb-3">
+                        <label for="select" className="form-label">Kelas Koding yang Dipilih</label>
+                        <select id="select" className="form-select" aria-label="Default select example" name="kelas" value={data.kelas} onChange={(e)=> changeValue(e)} >
                             <option value="" >Open this select menu</option>
                             <option value="backend">Coding Backend with Golang</option>
                             <option value="frontend">Coding Frontend with ReactJS</option>
                             <option value="fullstack">Fullstack Developer</option>
                         </select>
                     </div>
-                    <div class="mb-3">
+                    <div className="mb-3">
                         <label for="formFile" class="form-label">Foto Surat Kesungguhan</label>
-                        <input class="form-control" type="file" id="formFile" ref={suratKesungguhan}/>
+                        <input className="form-control" type="file" id="formFile" ref={suratKesungguhan}/>
                     </div>
-                    <div class="mb-3">
+                    <div className="mb-3">
                         <label for="InputHarapan" class="form-label">Harapan Untuk Coding Bootcamp ini</label>
-                        <textarea value={data.harapan} name="harapan" onChange={(e)=> changeValue(e)} class="form-control" id="InputHarapan" rows="3"></textarea>
+                        <textarea value={data.harapan} name="harapan" onChange={(e)=> changeValue(e)} className="form-control" id="InputHarapan" rows="3"></textarea>
                     </div>
-                    <div class="mb-3">
+                    <div className="mb-3">
                     <ul>
                     {(!errorMessage.nama ==="") &&
                     <li>{errorMessage.nama}</li>
