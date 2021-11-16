5c49119a42eab538980c8f29a95f11f3b890232b
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-11-16 11:42:57 +0700

    add filter by id feature

--- graphql_apollo/src/component/Home.js
+++ graphql_apollo/src/component/Home.js
@@ -5,24 +5,27 @@ import ListPassenger from './ListPassenger';
 import Header from './Header';
 import {
     useQuery,
-    gql
+    gql,
+    useLazyQuery
   } from "@apollo/client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PENGUNJUNG = gql`
-  query GetAnggota {
-    anggota {
-        id
-        nama
-        umur
-        jenis_kelamin
-      }
+query MyQuery($id: Int) {
+    anggota(where: {id: {_eq: $id}}) {
+      id
+      jenis_kelamin
+      nama
+      umur
+    }
   }
 `;
 
 function Home() {
-    const { loading, error, data } = useQuery(PENGUNJUNG);
+    const [getPengunjung, { loading, error, data }] = useLazyQuery(PENGUNJUNG,{ fetchPolicy: 'no-cache' });
+    const [id,setId] = useState("");
+    const [fetch,setFetch] = useState(false);
     const [list,setList] = useState([]);
     
     const hapusPengunjung = id => {
@@ -42,12 +45,31 @@ function Home() {
         setList([...list, newData]  
         );
     };
+
+    const onGetData = () => {
+        getPengunjung({variables:{
+            id: id,
+        }
+        })
+        setTimeout(() => {setFetch(true)}, 5000);
+        
+        
+    };
+
+    const onChangeId = (e) => {
+        if(e.target) {
+            setId(e.target.value)
+        }
+    };
+
         return (
             <div>
                 <Header/>
+                <input value={id} onChange={onChangeId}/>
+                <button onClick={() => onGetData()}>get list</button>
                 { loading && <h1>loading...</h1>}
                 { error && <h1>error...</h1>}
-                { !loading && 
+                { (!loading && !error && fetch) && 
                 <ListPassenger 
                 data={data.anggota}
                 hapusPengunjung={hapusPengunjung}/>
